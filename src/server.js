08b128c8e14dc7ebc40226cae463bb0afd65ce39
Sangheon Lee <wwdbsh@gmail.com>
2021-06-02 18:40:23 -0400

    introspection

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const apollo = new ApolloServer({
     resolvers,
     typeDefs,
     playground:true,
+    introspection:true,
     context: async ({ req }) => {
         return {
             loggedInUser:await getUser(req.headers.token),
