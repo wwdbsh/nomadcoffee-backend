d8134bc3818e018497070cd3a428c21387e813be
Sangheon Lee <wwdbsh@gmail.com>
2021-06-03 19:44:27 -0400

    20210603:nomad-coffee coffeeShop-util

--- src/coffeeShop/coffeeShop.utils.js
+++ src/coffeeShop/coffeeShop.utils.js
@@ -1,3 +1,7 @@
+import { FileUpload } from "graphql-upload";
+import { createWriteStream } from "fs";
+import path from "path";
+
 export const processCategories = categories => {
     return categories.map(category => ({
         where:{ name:category },
@@ -10,4 +14,14 @@ export const processPhotos = (photos) => {
         where:{ url },
         create:{ url }
     }));
+};
+
+export const handleFile = async (file, id) => {
+    const { filename, createReadStream } = await file;
+    const newFilename = `${id}-${Data.now()}-${filename}`;
+    const readStream = createReadStream();
+
+    const writeStream = createWriteStream(path.join(process.cwd(), "uploads", newFilename));
+    readStream.pipe(writeStream);
+    return `http://localhost:4000/static/${newFilename}`;
 };
